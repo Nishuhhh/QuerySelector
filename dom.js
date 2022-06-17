16f0d3232ac26fab2f2d66fbfc4b7fd68f776408
Nishuhhh <priyanshsharma2810@gmail.com>
2022-06-17 17:09:21 +0530

    the created changes

---
+++ dom.js
@@ -0,0 +1,11 @@
+//console.dir(document) ;
+
+
+//console.log(document.getElementById('header-title'));
+//var headertitle = document.getElementById('header-title') ;
+//var header = document.getElementById('main-header') ;
+//hconsole.log(headertitle);
+//header.style.font= 'solid 3px #000';
+var tt = document.querySelector('h2');
+tt.style.font = 'solid 3px ###2';
+tt.style.color = "green";
