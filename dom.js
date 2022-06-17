20224f52a967b56cd070b6303f4b0ca1a766a9d0
Nishuhhh <priyanshsharma2810@gmail.com>
2022-06-17 21:54:31 +0530

    adding second filegit

--- dom.js
+++ dom.js
@@ -6,6 +6,11 @@
 //var header = document.getElementById('main-header') ;
 //hconsole.log(headertitle);
 //header.style.font= 'solid 3px #000';
-var tt = document.querySelector('h2');
-tt.style.font = 'solid 3px ###2';
-tt.style.color = "green";
+//var tt = document.querySelector('h2');
+//tt.style.fontWeight = "900" ;
+//tt.style.color = "green";
+
+
+var items = document.getElementsByTagName('l1') ;
+items.style.backgroundColor = "red" ;
+
